a35117b8b271d3a00b7e020e27744386f3d39a39
Simone Orsi <simahawk@gmail.com>
2012-05-17 15:51:47 +0200

    1st upload

---
+++ __openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Advanced',
+    'version': '1.0',
+    'category': 'Web',
+    'description': """""",
+    'author': 'Simone Orsi - Domsense (simone.orsi@domsense.com)',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': ['web'],
+    'data': [],
+    'active': False,
+    'installable': True,
+    'web':True,
+    'css': [
+        'static/css/web_advanced_export.css',
+    ],
+    'js': [
+        'static/js/web_advanced_export.js',
+    ],
+    'xml': [
+        'static/xml/web_advanced_export.xml',
+    ],
+}
+
